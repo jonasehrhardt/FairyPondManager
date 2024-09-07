e0ef9267285c89505754111673d98beb1d4ef7db
Warriorroq <warriorroq@gmail.com>
2024-09-07 20:05:20 +0200

    add quest system

---
+++ Assets/Scripts/QuestSystem/QuestDealer.cs
@@ -0,0 +1,19 @@
+﻿using QuestSystem;
+using UnityEngine;
+
+namespace Assets.Scripts.QuestSystem
+{
+    public class QuestDealer : MonoBehaviour
+    {
+        public Quest quest;
+        public bool hasBeenGiven;
+        public void GiveQuest()
+        {
+            if (hasBeenGiven)
+                return;
+
+            hasBeenGiven = true;
+            QuestList.Instance.TakeQuest(quest);
+        }
+    }
+}
---
+++ Assets/Scripts/QuestSystem/WinningCondition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace QuestSystem
+{
+    [CreateAssetMenu(fileName = "QWC", menuName = "QuestSystem/WinningCondition", order = 0)]
+    public abstract class WinningCondition
+    {
+        public abstract bool IsDone();
+    }
+}
